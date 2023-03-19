ff136971594c48e8ace55a3c8bd937b4e25d74c7
TTRAN70 <107781508+TTRAN70@users.noreply.github.com>
2023-03-18 21:03:50 -0400

    added fcc test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
 import Drum from "./Drum.js";
+import ReactFCCtest from 'react-fcctest';
 
 export default function App() {
   return (
     <div>
+      <ReactFCCtest />
       <div className="wrapper">
         <Drum />
       </div>
